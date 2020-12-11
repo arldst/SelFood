5f1ee5e9acbc26a48030d705c8a1e979b3583a45
Ariel Jusuf <ariel.jusuf@gmail.com>
2020-12-11 21:58:55 +0700

    added register dialog span

--- app/src/main/java/com/kom5a_tugasbesar/selfood/Activity/LoginActivity.java
+++ app/src/main/java/com/kom5a_tugasbesar/selfood/Activity/LoginActivity.java
@@ -1,19 +1,31 @@
 package com.kom5a_tugasbesar.selfood.Activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
+import android.view.View;
 import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.kom5a_tugasbesar.selfood.Activity.ui.RegisterOptionFragment;
 import com.kom5a_tugasbesar.selfood.R;
 
 public class LoginActivity extends AppCompatActivity {
 
-    public TextView emailInput, passwordInput, registerText;
-    public Button loginPelangganBtn, loginRestoBtn;
-    public ProgressBar progressBar;
+    private TextView emailInput, passwordInput, registerText;
+    private Button loginPelangganBtn, loginRestoBtn;
+    private FrameLayout registerOptionFragment;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +37,32 @@ public class LoginActivity extends AppCompatActivity {
         loginPelangganBtn = findViewById(R.id.loginButtonUser);
         loginRestoBtn = findViewById(R.id.loginButtonResto);
         registerText = findViewById(R.id.loginRegisterButton);
+        registerOptionFragment = findViewById(R.id.loginRegisterOption);
         progressBar = findViewById(R.id.loginProgressBar);
 
+        String registerTextDialog = registerText.getText().toString();
+        SpannableString ss = new SpannableString(registerTextDialog);
+
+        ClickableSpan clickableSpan = new ClickableSpan() {
+            @Override
+            public void onClick(@NonNull View view) {
+                registerTextAction();
+                Toast.makeText(LoginActivity.this, "test", Toast.LENGTH_SHORT).show();
+            }
+        };
+
+        ss.setSpan(clickableSpan, 18, 27, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        registerText.setText(ss);
+        registerText.setMovementMethod(LinkMovementMethod.getInstance());
+
+    }
+
+    private void registerTextAction() {
+
+        RegisterOptionFragment fragment = RegisterOptionFragment.newInstance();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.add(R.id.loginRegisterOption, fragment, "REGISTER_OPTION_FRAGMENT");
     }
 }
