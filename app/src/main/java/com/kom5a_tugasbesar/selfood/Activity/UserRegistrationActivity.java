5b87efcd6117d4718fdb269c7d4711465c1cec02
Ariel Jusuf <ariel.jusuf@gmail.com>
2020-12-12 18:21:25 +0700

    added regis page

--- app/src/main/java/com/kom5a_tugasbesar/selfood/Activity/UserRegistrationActivity.java
+++ app/src/main/java/com/kom5a_tugasbesar/selfood/Activity/UserRegistrationActivity.java
@@ -3,14 +3,20 @@ package com.kom5a_tugasbesar.selfood.Activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 
 import com.kom5a_tugasbesar.selfood.R;
 
 public class UserRegistrationActivity extends AppCompatActivity {
 
+    private static final String[] restoCategory = new String[]{"Barat", "Cepat Saji", "Chinese", "Jepang", "Korea", "Seafood", "Timur Tengah", "Lainnnya"};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_registration);
+
     }
 }
